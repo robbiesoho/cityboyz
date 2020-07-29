0ad0ffcfe7f6169aeaefaa7cbd6cf50d9a8056d3
robbiesoho <rob89porto@gmail.com>
2020-07-29 08:11:30 -0400

    debugging deployment

--- client/src/components/Articles/Article.js
+++ client/src/components/Articles/Article.js
@@ -61,8 +61,14 @@ Article.propTypes = {
   article: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  article: state.article,
-});
+const mapStateToProps = (state) => {
+  console.log("My store state ===>", state); // log the `state` and see the exact path for articles
+  return {
+    article: state.article.articles,
+  };
+};
+// const mapStateToProps = (state) => ({
+//   article: state.article,
+// });
 
 export default connect(mapStateToProps, { getArticle, getAuthor })(Article);
