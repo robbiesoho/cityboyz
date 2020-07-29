3cd0b2325cef7484d1b3d57a0371688341ba2433
robbiesoho <rob89porto@gmail.com>
2020-07-29 14:42:03 -0400

    debug deployment

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -4,7 +4,7 @@ class Hero extends Component {
   render() {
     return (
       <div id="hero">
-        <div>
+        <div id="hero-container">
           <h1 id="hero-title">City Boyz</h1>
           <h1 id="little-media">Media</h1>
         </div>
